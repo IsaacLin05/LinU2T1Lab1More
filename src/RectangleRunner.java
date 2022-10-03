45e49245bba67b3dcdef4957e7bc43da7721a848
IsaacL05 <IsaacL9025@nycstudents.net>
2022-10-03 09:29:32 -0400

    Upgrade

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class RectangleRunner {
+public class RectangleRunner {
+    public static void main(String[] args)
+    {
+        int TotalArea;
+
+        Rectangle plot1 = new Rectangle(150, 200);
+        plot1.setWidth(75);
+        plot1.calculateArea();
+
+        Rectangle plot2 = new Rectangle(125);
+        plot2.setWidth(75);
+        plot2.setLength(75);
+        plot2.calculateArea();
+
+        Rectangle plot3 = new Rectangle();
+        plot3.setWidth(75);
+        plot3.calculateArea();
+
+        TotalArea=plot3.calculateArea() + plot1.calculateArea() + plot2.calculateArea();
+        System.out.println("The total amount of seeds requires " + TotalArea + " square foot of seeds");
+
+    }
 }
